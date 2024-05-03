9aeea6eb6c2082fa279b15ad5793d7f7a6a2174e
majorzsolti <168818986+majorzsolti@users.noreply.github.com>
2024-05-03 13:49:31 +0200

    Update syscheck.sh

--- syscheck.sh
+++ syscheck.sh
@@ -5,7 +5,6 @@ ubuntu_version=$(lsb_release -rs)
 echo ubuntu_version
 if [[ "$ubuntu_version" != "20.04" ]]; then
     echo "Error: This script requires Ubuntu 20.04 LTS ('Focal Fossa')."
-    exit 1
 fi
 
 # Check for CPU cores and AVX support
@@ -15,7 +14,6 @@ avx_support=$(grep -o avx /proc/cpuinfo | wc -l)
 echo avx_support
 if (( cpu_cores < 8 || avx_support == 0 )); then
     echo "Error: This script requires at least 8 vCPUs with AVX support."
-    exit 1
 fi
 
 # Check for RAM
@@ -23,7 +21,6 @@ total_ram=$(free -m | awk '/^Mem:/{print $2}')
 echo total_ram
 if (( total_ram < 16000 )); then
     echo "Error: This script requires at least 16 GB of RAM."
-    exit 1
 fi
 
 # Check for Free Disk Space
@@ -31,7 +28,6 @@ free_disk_space=$(df -BM --output=avail / | sed '1d;s/[^0-9]*//g')
 echo free_disk_space
 if (( free_disk_space < 32000 )); then
     echo "Error: This script requires at least 32 GB of free disk space."
-    exit 1
 fi
 
 echo "System requirements are met. You're good to go!"
