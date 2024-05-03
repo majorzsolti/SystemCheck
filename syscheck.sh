16da4fb24517aef8d08b4bc43a9c6043fdbfc6f8
majorzsolti <168818986+majorzsolti@users.noreply.github.com>
2024-05-03 15:35:12 +0200

    Update syscheck.sh

--- syscheck.sh
+++ syscheck.sh
@@ -73,16 +73,12 @@ if (( cpu_cores < 8 || avx_support == 0 )); then
 fi
 
 # RAM
-total_ram=$(free -m | awk '/^Mem:/{print $2}')
-echo $total_ram
 if (( total_ram < 15.5 )); then
     echo "Error: This script requires at least 16 GB of RAM."
     met_requirement='NO'
 fi
 
 # Free Disk Space
-free_disk_space=$(df -BM --output=avail / | sed '1d;s/[^0-9]*//g')
-echo $free_disk_space
 if (( free_disk_space < 31.5 )); then
     echo "Error: This script requires at least 32 GB of free disk space."
     met_requirement='NO'
