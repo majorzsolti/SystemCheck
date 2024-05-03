1b90f91e2a71737ed262fe0a8065717a3f132c31
majorzsolti <168818986+majorzsolti@users.noreply.github.com>
2024-05-03 14:07:45 +0200

    Update syscheck.sh

--- syscheck.sh
+++ syscheck.sh
@@ -1,6 +1,35 @@
 #!/bin/bash
 
+# Objective:
+#   Write a standalone script that checks if the current system meets the following minimum requirements:
+#   Ubuntu 20.04 LTS ("Focal Fossa"): later versions are not supported yet
+#   8 vCPUs: a CPU with AVX support is required
+#   16 GB RAM
+#   32 GB Free Disk Space
+
+# Task:
+#   Implement your solution in a common scripting language that is pre-installed on Ubuntu 20 (Bash or Python solutions are preferred)
+#   Feel free to use rounded values (e.g. 15.8 GB RAM is good enough)
+#   Check each requirement and output the current value on the system and the required value. Please format the output to be easily
+#   readable!
+#   Print a final verdict at the end: PASSED or FAILED (if we had at least one missing requirement)
+
 # Check for Ubuntu 20.04 LTS
+exho "Checnking if system meets minimum requirements"
+echo " " 
+
+ubuntu_version_minimum=$("20.04")
+ubuntu_version=$(lsb_release -rs)
+ubuntu_passed="YES"
+if [[ "$ubuntu_version" != "20.04" ]]; then
+    ubuntu_passed="NO"
+fi
+
+echo $ubuntu_version_minimum
+echo $ubuntu_version
+echo $ubuntu_passed
+
+echo "              Minimum requirements    System specification    Passed"
 ubuntu_version=$(lsb_release -rs)
 echo $ubuntu_version
 if [[ "$ubuntu_version" != "20.04" ]]; then
